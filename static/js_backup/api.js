e13036f39c8e5bd6a5bb112ecb4344f2a80ce667
Ayman Makram <aymanmmokhtar@gmail.com>
2026-02-04 03:07:02 +0200

    remove legacy static js duplicates/backups

--- static/js_backup/api.js
+++
@@ -1,8 +0,0 @@
-// static/js/api.js
-
-async function apiFetch(url, options = {}) {
-    options.headers = window.getAuthHeaders();
-    const response = await fetch(url, options);
-    if (window.handleAuthError(response)) return null;
-    return response;
-}
